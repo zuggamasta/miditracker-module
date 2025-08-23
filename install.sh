ced69a63af84917835ce8bf6f9b7f33448236f9b
zuggamasta <34277191+zuggamasta@users.noreply.github.com>
2025-08-23 20:59:16 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/bin/sh -e
-apt-get update
-apt-get install jack-sine -y
+git clone https://github.com/zuggamasta/midiTracker.git
+cd midiTracker/
+python3 -m venv venv/
+source venv/bin/activate
+python3 -m pip install mido[ports-rtmidi]
 echo "Done! Thank you!"
