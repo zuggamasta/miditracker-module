7673e37108104b5f309b6d776a46c4771cd1591b
zuggamasta <34277191+zuggamasta@users.noreply.github.com>
2025-08-23 21:14:21 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 #!/bin/sh -e
-git clone https://github.com/zuggamasta/midiTracker.git
+git clone  --depth 1 https://github.com/zuggamasta/midiTracker.git
+echo "Current directory"
+echo pwd
 cd midiTracker/
+echo "Current directory"
+echo pwd
 python3 -m venv venv/
+echo "Created venv, next up activating"
 source venv/bin/activate
 python3 -m pip install mido[ports-rtmidi]
 echo "Done! Thank you!"
